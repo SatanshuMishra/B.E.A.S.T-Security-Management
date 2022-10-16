4d489fc1879138dd9bb4524d49a6cc6800f13864
SatanshuMishra <satanshumishra@outlook.com>
2022-10-16 14:40:04 -0700

    VScode Project

---
+++ mySystem/src/Key.java
@@ -0,0 +1,13 @@
+import java.util.UUID;
+
+public class Key {
+	UUID id;
+	int clearanceLevel;
+	boolean isWorking = true;
+	
+	Key(int clearanceLevel){
+		this.id = UUID.randomUUID();
+		this.clearanceLevel = clearanceLevel;
+	}
+	
+}
---
+++ mySystem/src/User.java
@@ -0,0 +1,14 @@
+import java.util.UUID;
+
+public class User {
+	UUID id;
+	String firstName;
+	String lastName;
+	Key key;
+	
+	User(String firstName, String lastName){
+		this.id = UUID.randomUUID();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
