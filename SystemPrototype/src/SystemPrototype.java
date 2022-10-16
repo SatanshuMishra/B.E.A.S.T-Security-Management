58ff3fb31e5589b6f492d5e89b3763e952da0e1b
SatanshuMishra <satanshumishra@outlook.com>
2022-10-15 17:29:08 -0700

    Created Base Project
    I created the primary project. This can be branched.

---
+++ SystemPrototype/src/SystemPrototype.java
@@ -0,0 +1,9 @@
+
+public class SystemPrototype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- beast_project/src/beast_project/beast_project.java
+++
@@ -1,9 +0,0 @@
-package beast_project;
-
-public class beast_project {
-
-	public static void main(String[] args) {
-		System.out.print("Hello World!");
-	}
-
-}
